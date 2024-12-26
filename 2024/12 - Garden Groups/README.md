61338e3856e739d7f3d86cdbcb336df79114653d
sedith <martin.jacquet@posteo.net>
2024-12-27 00:50:41 +0100

    2024 12 p2

--- 2024/12 - Garden Groups/README.md
+++ 2024/12 - Garden Groups/README.md
@@ -8,4 +8,25 @@ The grid is parsed with two nested loops:
 * An iterative exploration of the region. The 4-neighbors for each new cell are checked for their crop type. Matching cells are added to the current list to explore.
 The area (nb of cells) and perimeter (sum of `4 - nb of matching neighbors`) are incrementally computed.
 
+Could be optimized by keeping track the direction from which the new cells are discovered, in order to recude the number of neighbor checks.
+
 ## Part 2
+
+The main thing to note is that the number of sides of the polytopes is equal to its number of corners. Therefore, it is only about assessing cells which are corners.
+There are two corner types only:
+1. "outward" corners, where two neighbors 90° appart are in a different region (or outside of the grid), eg the central cell in
+```
+BBB
+AAB
+AAB
+```
+1. "inward" corners where two neighbors 90° appart are in the same region (qnd not ouside the grid) but the diagonal neighbor inbetween is in a different region, eg the central cell in
+```
+AAA
+AAA
+AAB
+```
+
+The implementation is similar to part one but now with a 8-neighbor `Enum`.
+The added function `is_corner(p, grid)` checks if `p` is a corner according to any of the two criteria.
+Note that the function returns the "multiplicity" of the corner, since a single cell can be a corner for several sides. It is notably the case for an area made of a single cell, which has a "multiplicity" of 4.
--- 2024/12 - Garden Groups/part2.py
+++ 2024/12 - Garden Groups/part2.py
@@ -3,21 +3,22 @@ from enum import Enum
 
 class Dir(Enum):
     N = 0
-    E = 1
-    S = 2
-    W = 3
+    NE = 1
+    E = 2
+    SE = 3
+    S = 4
+    SW = 5
+    W = 6
+    NW = 7
 
     def cw(self):
-        return Dir((self.value + 1) % 4)
+        return Dir((self.value + 1) % 8)
 
     def ccw(self):
-        return Dir((self.value - 1) % 4)
+        return Dir((self.value - 1) % 8)
 
     def move(self):
-        return ((-1, 0), (0, 1), (1, 0), (0, -1))[self.value]
-
-    def __str__(self):
-        return ('^', '>', 'v', '<')[self.value]
+        return ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))[self.value]
 
 
 class Pos(tuple):
@@ -66,11 +67,33 @@ class Grid:
             return next
 
 
+def is_corner(p, grid):
+    type = grid.get(p)[1]
+    quarters = [
+        [Dir.N, Dir.NE, Dir.E],
+        [Dir.E, Dir.SE, Dir.S],
+        [Dir.S, Dir.SW, Dir.W],
+        [Dir.W, Dir.NW, Dir.N],
+    ]
+    return sum([
+        (
+            (p + d0 is None or grid.get(p + d0)[1] != type) and
+            (p + d2 is None or grid.get(p + d2)[1] != type)
+        ) or
+        (
+            p + d0 is not None and grid.get(p + d0)[1] == type and
+            p + d1 is not None and grid.get(p + d1)[1] != type and
+            p + d2 is not None and grid.get(p + d2)[1] == type
+        )
+        for d0, d1, d2 in quarters
+    ])
+
+
 class Region:
     def __init__(self, type):
         self.type = type
         self.area = 0
-        self.peri = 0
+        self.sides = 0
 
 
 def main(data):
@@ -87,11 +110,12 @@ def main(data):
                 valid_neighbors = [p for p in [pos + d for d in [Dir.N, Dir.E, Dir.S, Dir.W]] if p and grid.get(p)[1] == crop_type]
                 to_be_regionned.update([p for p in valid_neighbors if not grid.get(p)[0]])
                 region.area += 1
-                region.peri += 4 - len(valid_neighbors)
+                region.sides += is_corner(pos, grid)
             regions.append(region)
 
-    # [print(f'region of {r.type}, area {r.area}, perimeter {r.peri}, price {r.area * r.peri}') for r in regions]
-    return sum([r.area * r.peri for r in regions])
+
+    # [print(f'region of {r.type}, area {r.area}, sides {r.sides}') for r in regions]
+    return sum([r.area * r.sides for r in regions])
 
 
 if __name__ == '__main__':
